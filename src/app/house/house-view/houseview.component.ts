4b76f678f59421f544f94ff75baee3243cf2f985
IshworKhadka <ishworfloyd@gmail.com>
2022-01-25 13:14:10 +0545

    house photo preview

--- src/app/house/house-view/houseview.component.ts
+++ src/app/house/house-view/houseview.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 import { GlobalConstants } from 'src/app/global-constants';
 import { HouseModel } from 'src/app/models/house';
 import { Apiservice } from '../../api.service';
@@ -12,6 +13,13 @@ import { Apiservice } from '../../api.service';
   styleUrls: ['../house.component.css'],
 })
 export class HouseViewComponent {
+  selectedFiles?: FileList;
+  progressInfos: any[] = [];
+  messages: string[] = [];
+
+  previews: string[] = [];
+  imageInfos?: Observable<any>;
+
   imageUrl: string = '/assets/img/';
 
   house_types = GlobalConstants.house_types;
@@ -36,35 +44,95 @@ export class HouseViewComponent {
     }
   }
 
-  public uploadFile = (files: any) => {
-    if (files.length == 0) {
-      return;
+  onFileChange(event : any) {
+    this.messages = [];
+    this.progressInfos = [];
+    this.selectedFiles = event.target.files;
+
+    this.previews = [];
+    if (this.selectedFiles && this.selectedFiles[0]) {
+      const numberOfFiles = this.selectedFiles.length;
+      for (let i = 0; i < numberOfFiles; i++) {
+        const reader = new FileReader();
+
+        reader.onload = (e: any) => {
+          this.previews.push(e.target.result);
+        };
+
+        reader.readAsDataURL(this.selectedFiles[i]);
+      }
     }
+  }
 
-    let filesTeUpload: File[] = files;
-    const formData = new FormData();
+  removeImage(idx: number){
+    for(var i =0; i < this.previews.length; i++){
+      if(i === idx){
+        this.previews.splice(i,1);
+      }
+    }
+  }
 
-    Array.from(filesTeUpload).map((file, index) => {
-      return formData.append('file' + index, file, file.name);
-    });
-    console.log(formData)
+  uploadFiles(): void {
+    this.messages = [];
+  
+    if (this.selectedFiles) {
+      for (let i = 0; i < this.selectedFiles.length; i++) {
+        this.upload(i, this.selectedFiles[i]);
+      }
+    }
+  }
 
-    this.http
-      .post(GlobalConstants.BaseURI + '/api/house', formData, {
-        reportProgress: true,
-        observe: 'events',
-      })
-      .subscribe((event) => {
-        if (event.type === HttpEventType.UploadProgress) {
-          this.progress = Math.round(
-            (100 * event.loaded) / (event.total as number)
-          );
-        } else if (event.type === HttpEventType.Response) {
-          this.message = 'Upload Successful';
-          this.onUploadFinished.emit(event.body);
-        }
-      });
-  };
+  upload(idx: number, file: File): void {
+    this.progressInfos[idx] = { value: 0, fileName: file.name };
+  
+    if (file) {
+      this.http.post(GlobalConstants.BaseURI + '/api/house', file).subscribe({
+        next: (event: any) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progressInfos[idx].value = Math.round(100 * event.loaded / event.total);
+          } else if (event instanceof HttpResponse) {
+            const msg = 'Uploaded the file successfully: ' + file.name;
+            this.messages.push(msg);
+            //this.imageInfos = this.uploadService.getFiles();
+          }
+        },
+        error: (err: any) => {
+          this.progressInfos[idx].value = 0;
+          const msg = 'Could not upload the file: ' + file.name;
+          this.messages.push(msg);
+        }});
+    }
+  }
+
+  // public uploadFile = () => {
+  //   if (files.length == 0) {
+  //     return;
+  //   }
+
+  //   let filesTeUpload: File[] = files;
+  //   const formData = new FormData();
+
+  //   Array.from(filesTeUpload).map((file, index) => {
+  //     return formData.append('file' + index, file, file.name);
+  //   });
+  //   console.log(formData)
+
+  //   this.http
+  //     .post(GlobalConstants.BaseURI + '/api/house', formData, {
+  //       reportProgress: true,
+  //       observe: 'events',
+  //     })
+  //     .subscribe((event) => {
+  //       if (event.type === HttpEventType.UploadProgress) {
+  //         this.progress = Math.round(
+  //           (100 * event.loaded) / (event.total as number)
+  //         );
+  //       } else if (event.type === HttpEventType.Response) {
+  //         this.message = 'Upload Successful';
+  //         this.onUploadFinished.emit(event.body);
+  //       }
+  //     });
+  // };
 
   put(model: HouseModel){
     this.api.putHouseDetails(model)
